041f1ea98b4c9a035bdaaf4fa9e07bc6199d12e9
lucas <luczssz3@gmail.com>
2026-03-27 09:50:38 -0300

    feat: declaraçoes e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// DECLARAÇÕES E VARIAVEIS
+//VAR, LET E CONST
+
+var nome="fiap";
+console.log(nome);
+var nome="Dev";
+console.log(nome);
+
+if(true){
+    var avatar="fiapinho"
+}
+
+console.log(avatar);
